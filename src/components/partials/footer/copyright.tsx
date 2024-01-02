e42f9a4c7d70982658b14ad4403a9b965d9de55b
Semet <hamdanilombok@gmail.com>
2024-01-02 21:11:52 +0800

    change footer emoji

--- src/components/partials/footer/copyright.tsx
+++ src/components/partials/footer/copyright.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Copyright = () => {
    return (
       <div className="flex justify-center -mt-4 py-2 text-heading dark:text-slate-200">
-         <span>©{new Date().getFullYear()} made with ♥️ by Hamdani Ash-Sidiq</span>
+         <span>©{new Date().getFullYear()} made with ❤️ by Hamdani Ash-Sidiq</span>
       </div>
    );
 };
