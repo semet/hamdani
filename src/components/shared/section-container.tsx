7464e2ab84dba21f01b9ab48aaaee14265bd68bb
hamdani <hamdanilombok@gmail.com>
2024-01-09 04:39:32 +0700

    fix margin overflowing

--- src/components/shared/section-container.tsx
+++ src/components/shared/section-container.tsx
@@ -18,12 +18,10 @@ const SectionContainer: FC<Props> = ({ id, subtitle, title, children }) => {
    const ref = useRef<HTMLSpanElement>(null);
    const isInView = useInView(ref);
    const mainControl = useAnimation();
-   const slideControl = useAnimation();
 
    useEffect(() => {
       if (isInView) {
          mainControl.start("visible");
-         slideControl.start("visible");
       }
    });
    return (
@@ -32,32 +30,23 @@ const SectionContainer: FC<Props> = ({ id, subtitle, title, children }) => {
          className="main-padding mt-32 space-y-12 min-w-full"
          id={id}
       >
-         <div className="text-center space-y-5 relative">
-            <motion.h3
-               variants={{
-                  hidden: { opacity: 0, y: 75 },
-                  visible: { opacity: 1, y: 0 },
-               }}
-               initial="hidden"
-               animate={mainControl}
-               transition={{ duration: 0.5, delay: 0.25 }}
-               className="text-4xl xl:text-5xl font-extrabold text-heading dark:text-slate-300 inline-block overflow-x-clip relative"
-            >
+         <motion.div
+            variants={{
+               hidden: { opacity: 0, y: 75 },
+               visible: { opacity: 1, y: 0 },
+            }}
+            initial="hidden"
+            animate={mainControl}
+            transition={{ duration: 0.5, delay: 0.25 }}
+            className="text-center space-y-5 relative"
+         >
+            <h3 className="text-4xl xl:text-5xl font-extrabold text-heading dark:text-slate-300 inline-block overflow-x-clip relative">
                {title}
-               <motion.span
-                  variants={{
-                     hidden: { left: 0 },
-                     visible: { left: "100%" },
-                  }}
-                  animate={slideControl}
-                  transition={{ duration: 1, ease: "easeIn" }}
-                  className="absolute top-0 left-0 right-0 transform origin-left z-50 place-self-start bg-slate-900/45 dark:bg-slate-300 h-full w-full"
-               ></motion.span>
-            </motion.h3>
+            </h3>
             <p className="text-slate-600 dark:text-slate-400 text-base lg:text-xl">
                {subtitle}
             </p>
-         </div>
+         </motion.div>
          {children}
       </section>
    );
